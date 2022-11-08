e9f0caabd73c45d413db36dc3ef0fcee965b62f7
Mazharul Islam <mazharul430@gmail.com>
2022-11-08 09:16:39 -0800

    get one data from database

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors());
@@ -12,41 +12,47 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qm6ghoc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-const run = async ()=>{
-    try{
+const run = async () => {
+    try {
         const serviceCollection = client.db('journalistService').collection('services');
 
-        // get three service data from database 
-        app.get('/services', async (req, res)=>{
-            const query = {};
-            const cursor = serviceCollection.find(query).limit(3);
-            const services = await cursor.toArray();
-            res.send(services);
-        })
-
-        
         // get all service data from database 
-        app.get('/services', async (req, res)=>{
+        app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
 
+        // get three service data from database 
+         app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        })
 
+        // find one service data in all services from database
+        app.get('/services/:id', async (req, res)=> {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+        
     }
-    finally{
+    finally {
 
     }
 }
 
-run().catch(err=> console.log(err));
+run().catch(err => console.log(err));
 
 
 app.get('/', (req, res) => {
-  res.send('Jornalist Server is Running !')
+    res.send('Jornalist Server is Running !')
 })
 
 app.listen(port, () => {
-  console.log(`Jornalist Server is Running on port ${port}`)
+    console.log(`Jornalist Server is Running on port ${port}`)
 })
