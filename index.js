3a18317e6b3f166267bc0eb30718901bf0e18eec
Mazharul Islam <mazharul430@gmail.com>
2022-11-08 03:03:40 -0800

    added get data from database

--- index.js
+++ index.js
@@ -3,12 +3,34 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qm6ghoc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const run = async ()=>{
+    try{
+        const serviceCollection = client.db('journalistService').collection('services');
+
+        // get data from database 
+        app.get('/services', async (req, res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(err=> console.log(err));
 
 
 app.get('/', (req, res) => {
